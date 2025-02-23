4c6d25a9259bc04229b1482b3361766dd47b126a
saphalpdyl <saphalpdyl@gmail.com>
2025-02-23 00:42:20 -0600

    feat(game): update ground model to use snowy terrain

--- apps/web/components/three/ground.tsx
+++ apps/web/components/three/ground.tsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Ground() {
-  const scene = useGLTF("/table_green_plains.glb");
+  const scene = useGLTF("/table_snow.glb");
   return <group scale={[1.4,1.4,1.4]} position={[0, -1.25, 0]} rotation={[0, Math.PI / 2, 0]}>
     <primitive object={scene.scene} />
   </group>;
