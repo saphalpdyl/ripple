089191b9fac0f75d267372952818cebf5650c34e
saphalpdyl <saphalpdyl@gmail.com>
2025-02-23 09:31:00 -0600

    feat(page): add Ripple logo and company name to the main page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Page() {
-  return <h1>Page</h1>
+  return <div className="h-screen w-screen flex-col p-4">
+    <div className="flex flex-col">
+      <Image src="/ripple_logo.png" alt="absd" width={100} height={100} />
+      <p className="pl-1 text-gray-400">Ripple Inc.</p>
+    </div>
+  </div>
 }
